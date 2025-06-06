9a5dcab4b3e6015d0e3466c00d8acfd3a194cc5f
Luan Brandao <brandaoluan506@gmail.com>
2025-06-06 00:15:21 -0300

    Criando Transição de Fase

---
+++ Assets/Scenes/TransicaoDeFase.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransicaoDeFase : MonoBehaviour
+{
+    public string nomeDaFaseDestino = "Fase2";
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(nomeDaFaseDestino);
+        }
+    }
+}
